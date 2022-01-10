76e2a2a4cbc45f4832673e15998738b7b12d093e
Anna <anna.vm@mail.ru>
2022-01-10 20:03:22 +0300

    Добавьте файлы проекта.

---
+++ Wpf 9/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf_8
+{
+    class MyCommands
+    {
+        public static RoutedCommand Unlock { get; set; }
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.U, ModifierKeys.Alt, "Alt+U"));
+            Unlock = new RoutedCommand("Открыть", typeof(MyCommands), inputs);
+        }
+    }
+}
